b3f5f35251758edb7f8e3879576f5cb46f76097e
Mali Brunk <MBrunk6875@student.whatcom.edu>
2024-01-22 12:48:19 -0800

    Working word search generator

--- MBAssignment1.java
+++ MBAssignment1.java
@@ -10,14 +10,13 @@
 
 import java.util.*;
 
-
-
 public class MBAssignment1 {
     public static final String ALL_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final int MAX_SEARCHES = 100;
     public final String[] directions = {"VERTICAL", "HORIZONTAL", "POSITIVE_LINEAR", "NEGATIVE_LINEAR"};
     public static void main(String[] args) {
         char[][] grid = new char[100][100];
+        char[][] copyGrid = new char[100][100];
         boolean isDone = false;
         boolean hasRun = false;
         Scanner input = new Scanner(System.in);
@@ -30,6 +29,7 @@ public class MBAssignment1 {
             switch (command) {
                 case 'c':
                     grid = generate(input);
+                    copyGrid = saveGridCopy(grid);
                     hasRun = true;
                     break;
                 case 'p':
@@ -40,7 +40,11 @@ public class MBAssignment1 {
                     print(grid);
                     break;
                 case 's':
-                    showSolution(grid);
+                if (!hasRun) {
+                    System.out.println("Please generate a word search first");
+                    break;
+                }
+                    showSolution(copyGrid);
                     break;
                 case 'q':
                     System.out.println("Thanks for playing!");
@@ -105,76 +109,83 @@ public class MBAssignment1 {
         switch (direction) {
             case 1: // vertical
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row + i][col] == 0) {
-                        grid[row + i][col] = word.charAt(i);
-                    }  else {
+                    if (grid[row + i][col] != 0) {
                         return false;
                     }
+                }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row + i][col] = word.charAt(i);
                 }
                 return true;
             case 2: // backwards vertical
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row - i][col] == 0) {
-                        grid[row - i][col] = word.charAt(i);
-                    } else {
+                    if (grid[row - i][col] != 0) {
                         return false;
                     }
+                }
+
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row - i][col] = word.charAt(i);
                 }
                 return true;
             case 3: // horizontal
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row][col + i] == 0) {
-                        grid[row][col + i] = word.charAt(i);
-                    } else {
+                    if (grid[row][col + i] != 0) {
                         return false;
                     }
-                return true;
+                }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row][col + i] = word.charAt(i);
                 }
                 return true;
             case 4: // backwards horizontal
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row][col - i] == 0) {
-                        grid[row][col - i] = word.charAt(i);
-                    } else {
+                    if (grid[row][col - i] != 0) {
                         return false;
                     }
+                }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row][col - i] = word.charAt(i);
                 }
                 return true;
             case 5: // diagonal /
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row + i][col + i] == 0) {
-                        grid[row + i][col + i] = word.charAt(i);
-                    } else {
+                    if (grid[row + i][col + i] != 0) {
                         return false;
                     }
-                return true;
+                }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row + i][col + i] = word.charAt(i);
                 }
                 return true;
             case 6: // backwards diagonal /
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row - i][col + i] == 0) {
-                        grid[row - i][col + i] = word.charAt(i);
-                    } else {
+                    if (grid[row + i][col - i] != 0) {
                         return false;
                     }
+                }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row + i][col - i] = word.charAt(i);
                 }
                 return true;
             case 7: // diagonal \
                 for (int i = 0; i < word.length(); i++) {
-                    if (grid[row + i][col - 1] == 0) {
-                        grid[row + i][col - 1] = word.charAt(i);
-                    } else {
+                    if (grid[row - i][col + i] != 0) {
                         return false;
                     }
                 }
+                for (int i = 0; i < word.length(); i++) {
+                    grid[row - i][col + i] = word.charAt(i);
+                }
                 return true;
             case 8: // backwards diagonal \
-                for ( int i = 0; i < word.length(); i++) {
-                    if (grid[row - 1][col - 1] == 0) {
-                        grid[row - 1][col - 1] = word.charAt(i);
-                    } else {
+                for (int i = 0; i < word.length(); i++) {
+                    if (grid[row - i][col - i] != 0) {
                         return false;
                     }
+                }
+                for ( int i = 0; i < word.length(); i++) {
+                    grid[row - i][col - i] = word.charAt(i);
                 }
                 return true;
         }
@@ -212,11 +223,11 @@ public class MBAssignment1 {
                     return 5;
                 }
             case 6: // backwards diagonal /
-                if ((row - word.length() > 0) && (col + word.length() < grid.length)) {
+                if ((row + word.length() < grid.length) && (col - word.length() > 0)) {
                     return 6;
                 }
             case 7: // diagonal \
-                if ((row + word.length() < grid.length) && (col - word.length() > 0)) {
+                if ((row - word.length() > 0) && (col + word.length() < grid.length)) {
                     return 7;
                 }
             case 8: // backwards diagonal \
@@ -228,11 +239,22 @@ public class MBAssignment1 {
             }
     }
 
-    public static void fill(char[][] grid) {
+    public static char[][] saveGridCopy(char[][] grid) {
+        char[][] copyGrid = new char[grid.length][grid.length];
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[1].length; j++) {
+            for (int j = 0; j < grid.length; j++) {
+                copyGrid[i][j] = grid[i][j];
+            }
+        }
+        return copyGrid;
+    }
+
+    public static void fill(char[][] grid) {
+        Random rand = new Random();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
                 if (grid[i][j] == 0) {
-                    grid[i][j] = 'X';
+                    grid[i][j] = ALL_CHARS.charAt(rand.nextInt(ALL_CHARS.length()));
                 }
             }
         }
@@ -249,6 +271,19 @@ public class MBAssignment1 {
         }
     }
     public static void showSolution(char[][] grid) {
-        System.out.println("test");
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] == 0) {
+                    grid[i][j] = 'X';
+                }
+            }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[1].length; j++) {
+                System.out.print(grid[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 }
